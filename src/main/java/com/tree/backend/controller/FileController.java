1b3e07395cf9ecff3301b6e31a8c667d4f78f9a4
TheTree <1797978049@qq.com>
2023-10-09 10:25:16 +0800

    后端完整模板

--- src/main/java/com/tree/backend/controller/FileController.java
+++ src/main/java/com/tree/backend/controller/FileController.java
@@ -26,9 +26,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 文件接口
- *
- * @author <a href="https://github.com/litree">程序员鱼皮</a>
- * @from <a href="https://tree.icu">编程导航知识星球</a>
  */
 @RestController
 @RequestMapping("/file")
