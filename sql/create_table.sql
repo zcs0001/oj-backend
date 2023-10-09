8aecb1a847e5b1c5d764c5defd453c1b14fbf766
TheTree <1797978049@qq.com>
2023-10-09 13:06:31 +0800

    后端完整模板

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -22,42 +22,58 @@ create table if not exists user
     index idx_unionId (unionId)
 ) comment '用户' collate = utf8mb4_unicode_ci;
 
--- 帖子表
-create table if not exists post
+-- 用户编号表
+create table if not exists user_code
 (
     id         bigint auto_increment comment 'id' primary key,
-    title      varchar(512)                       null comment '标题',
-    content    text                               null comment '内容',
-    tags       varchar(1024)                      null comment '标签列表（json 数组）',
-    thumbNum   int      default 0                 not null comment '点赞数',
-    favourNum  int      default 0                 not null comment '收藏数',
-    userId     bigint                             not null comment '创建用户 id',
+    userId     bigint                             not null comment '用户id',
     createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
     updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     isDelete   tinyint  default 0                 not null comment '是否删除',
-    index idx_userId (userId)
-) comment '帖子' collate = utf8mb4_unicode_ci;
+    index idx_userAccount (userId)
+) comment '用户编号表' collate = utf8mb4_unicode_ci;
 
--- 帖子点赞表（硬删除）
-create table if not exists post_thumb
+# 使用json存储数据的前提
+# 1.不需要根据某个字段去倒查这条数据
+# 2.一个对象中的所有字段含义相关，属于同一类的值
+# 3.字段存储空间占用不同太大
+-- 题目表
+create table if not exists question
 (
-    id         bigint auto_increment comment 'id' primary key,
-    postId     bigint                             not null comment '帖子 id',
-    userId     bigint                             not null comment '创建用户 id',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    index idx_postId (postId),
+    id          bigint auto_increment comment 'id' primary key,
+    userId      bigint                             not null comment '创建题目用户 id',
+    title       varchar(512)                       null comment '标题',
+    content     text                               null comment '内容',
+    tags        varchar(1024)                      null comment '标签列表（json 数组）',
+    answer      text                               null comment '题目答案',
+-- 将例如输入输出用例放到一个对象中input output
+    judgeCase   text                               null comment '判题用例（json 数组）',
+-- 时间、空间、内存等配置放到一个对象中timeLimit stackLimit
+    judgeConfig text                               null comment '判题配置（json 对象）',
+    submitNum   int      default 0                 not null comment '题目提交数',
+    acceptedNum int      default 0                 not null comment '题目通过数',
+    thumbNum    int      default 0                 not null comment '点赞数',
+    favourNum   int      default 0                 not null comment '收藏数',
+    createTime  datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime  datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete    tinyint  default 0                 not null comment '是否删除',
     index idx_userId (userId)
-) comment '帖子点赞';
+) comment '题目' collate = utf8mb4_unicode_ci;
 
--- 帖子收藏表（硬删除）
-create table if not exists post_favour
+-- 题目提交表
+create table if not exists question_submit
 (
-    id         bigint auto_increment comment 'id' primary key,
-    postId     bigint                             not null comment '帖子 id',
-    userId     bigint                             not null comment '创建用户 id',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    index idx_postId (postId),
+    id             bigint auto_increment comment 'id' primary key,
+    questionId     bigint                             not null comment '题目 id',
+    userId         bigint                             not null comment '创建用户 id',
+    judgeInfo      text                               null comment '判题信息（json 对象）',
+    submitLanguage varchar(128)                       not null comment '编程语言',
+    submitCode     text                               not null comment '用户提交代码',
+    submitState    int      default 0                 not null comment '判题状态（0 - 待判题、1 - 判题中、2 - 成功、3 - 失败）',
+    createTime     datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime     datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete       tinyint  default 0                 not null comment '是否删除',
+    index idx_questionId (questionId),
     index idx_userId (userId)
-) comment '帖子收藏';
+) comment '题目提交';
+
