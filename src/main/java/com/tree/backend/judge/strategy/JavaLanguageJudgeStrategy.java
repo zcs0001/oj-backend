ddeb2f8dc3257d2f24a7ed812abc09920f43314b
thetree <1797978049@qq.com>
2023-10-26 13:28:49 +0800

    完整单体项目

--- src/main/java/com/tree/backend/judge/strategy/JavaLanguageJudgeStrategy.java
+++ src/main/java/com/tree/backend/judge/strategy/JavaLanguageJudgeStrategy.java
@@ -36,7 +36,7 @@ public class JavaLanguageJudgeStrategy implements JudgeStrategy{
         // 先判断沙箱执行的结果输出数量是否和预期输出数量相等
         if (outputList.size() != inputList.size()) {
             judgeInfoMessageEnum = JudgeInfoMessageEnum.WRONG_ANSWER;
-            judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
+            judgeInfoResponse.setMessage(judgeInfoMessageEnum.getText());
             return judgeInfoResponse;
         }
         // 依次判断每一项输出和预期输出是否相等
@@ -44,7 +44,7 @@ public class JavaLanguageJudgeStrategy implements JudgeStrategy{
             JudgeCase judgeCase = judgeCaseList.get(i);
             if (!judgeCase.getOutput().equals(outputList.get(i))) {
                 judgeInfoMessageEnum = JudgeInfoMessageEnum.WRONG_ANSWER;
-                judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
+                judgeInfoResponse.setMessage(judgeInfoMessageEnum.getText());
                 return judgeInfoResponse;
             }
         }
@@ -55,17 +55,18 @@ public class JavaLanguageJudgeStrategy implements JudgeStrategy{
         Long needTimeLimit = judgeConfig.getTimeLimit();
         if (executedMemory > needMemoryLimit) {
             judgeInfoMessageEnum = JudgeInfoMessageEnum.MEMORY_LIMIT_EXCEEDED;
-            judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
+            judgeInfoResponse.setMessage(judgeInfoMessageEnum.getText());
             return judgeInfoResponse;
         }
         // Java 程序本身需要额外执行 5 秒钟
         long JAVA_PROGRAM_TIME_COST = 5000L;
         if ((executedTime - JAVA_PROGRAM_TIME_COST) > needTimeLimit) {
             judgeInfoMessageEnum = JudgeInfoMessageEnum.TIME_LIMIT_EXCEEDED;
-            judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
+            judgeInfoResponse.setMessage(judgeInfoMessageEnum.getText());
             return judgeInfoResponse;
         }
-        judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
+//        judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
+        judgeInfoResponse.setMessage(judgeInfoMessageEnum.getText());
         return judgeInfoResponse;
     }
 }
